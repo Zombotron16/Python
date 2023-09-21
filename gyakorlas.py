bd146d52652e23d60c13cee8bcaa12c8d153e4dc
Zombotron16 <144986775+Zombotron16@users.noreply.github.com>
2023-09-21 08:20:23 +0200

    Add files via upload

---
+++ gyakorlas.py
@@ -0,0 +1,59 @@
+"""
+#1.feladat kérjünk be a felhasználótól 2 db egész számot és vizsgáljuk meg a következőt:Melyik a nagyobb érték,
+# a 2 érték =
+
+#2.feladat bekérünk a felhasználótol 5 db számot mindegyiket megvizsgáljuk hogy pozitív-e vagy negatív és hogy nagyobb-e vagy kisebb ?
+#bónusz feladat számold meg hány olyan van az 5 közül hogy hány db ami pozitív
+
+
+szam1 = int(input("adjon meg egy egész számot:"))
+
+szam2 = int(input("adjon meg egy másik egész számot:"))
+
+if szam1 > szam2:
+    print( szam1, " Nagyobb mint:", szam2)
+
+if szam2 > szam1:
+    print(szam2,"Nagyobb mint:", szam1)
+  """  
+
+szam1 = int(input("adjon meg egy egész számot:"))
+
+szam2 = int(input("adjon meg egy egész számot:"))
+
+szam3 = int(input("adjon meg egy egész számot:"))
+
+szam4 = int(input("adjon meg egy egész számot:"))
+
+szam5 = int(input("adjon meg egy egész számot:"))
+
+if szam1> 0:
+    print("pozitív")
+else:
+    print("negatív")
+    
+if szam2> 0:
+    print("pozitív")
+else:
+    print("negatív")
+
+if szam3> 0:
+    print("pozitív")
+
+if szam4> 0:
+    print("pozitív")
+else:
+    print("negatív")
+
+if szam5 > 0:
+    print("pozitív")
+
+else:
+    print("negatív")
+
+
+
+
+
+
+
